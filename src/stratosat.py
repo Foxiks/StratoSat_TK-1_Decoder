338d7188b30d9f5d86a3aa3b0e2dc670f1a6823e
Фукиш <egor158741@yandex.ru>
2024-02-17 17:51:12 +0300

    New offset func + HR mode

--- src/stratosat.py
+++ src/stratosat.py
@@ -1,9 +1,6 @@
 import sys, socket, argparse, time, bitstring
 from datetime import datetime
 
-name=f'Delete_this_file_{time.strftime("%m-%d_%H-%M")}'
-reg=0
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help="port")
 parser.add_argument("-ip", "--ip", help="ip")
@@ -85,53 +82,85 @@ def telemetry_decoder(data):
         out_tlm_file.write('Number of received packets: '+str(all_packets_rx)+'\n')
         out_tlm_file.write('Number of transmitted packets: '+str(all_packets_tx)+'\n')
 
-def main(s):
-    global reg, name
+def main(s,name):
     while True:
         frame = s.recv(2048).hex()
         frame = frame[74:]
         reply = [frame[i:i+2] for i in range(0, len(frame), 2)]
         frame = ' '.join(reply)
         img_sync = frame[:11]
-        if(int(str(frame.find(' ff d8 ff db '))) >= int(0)):
-            chb1=frame[21:23]
-            chb2=frame[18:20]
-            chb3=frame[15:17]
-            reg=bitstring.BitStream(hex=str(str(chb1)+str(chb2)+str(chb3))).read('uint')
-            name=str(time.strftime("%m-%d_%H-%M-%S"))
-            x=int(str(frame[23:].find(' ff d8 ')))
-            with open('out_image_'+str(name)+'.jpg', 'ab') as out_file:
-                bitstring.BitArray(hex=str(str(frame[23+x:]).replace(' ', ''))).tofile(out_file)
-            with open('data.ts', 'w') as o:
-                o.write('out_image_'+str(name)+'.jpg')
-        elif(str(img_sync) == str('02 00 3e 05')):    
-            chb1=frame[21:23]
-            chb2=frame[18:20]
-            chb3=frame[15:17]
-            check_value=bitstring.BitStream(hex=str(str(chb1)+str(chb2)+str(chb3))).read('uint')
-            print(f"Frame: {(int(check_value))}", end='\r')
-            if(int(check_value)==int(reg+56)):
-                with open('out_image_'+str(name)+'.jpg', 'ab') as out_file:
-                    bitstring.BitArray(hex=str(str(frame[23:]).replace(' ', ''))).tofile(out_file)
-                reg+=56
-            else:
-                skipped=int(int(check_value-reg)/56)
-                lenz=112*int(skipped)
-                if(lenz == 224):
-                    lenz = 112
-                with open('out_image_'+str(name)+'.jpg', 'ab') as out_file:
-                    bitstring.BitArray(hex=str(str('0'*lenz))).tofile(out_file)
-                    bitstring.BitArray(hex=str(str(frame[23:]).replace(' ', ''))).tofile(out_file)
-                reg+=int(56*int(skipped))
-        if(int(str(frame[23:].find(' ff d9 '))) >= int(0)):
-            with open('out_image_'+str(name)+'.jpg', 'ab') as out_file:
-                bitstring.BitArray(hex=str(str(frame[23:]).replace(' ', ''))).tofile(out_file)
+        if(str(img_sync) == str('02 00 3e 05')):
+            if(int(str(frame.find(' ff d8 ff db '))) >= int(0)):
+                first_offset=bitstring.BitStream(hex=str(frame[15:23])).read('uintle')
+                name=str(time.strftime("%m-%d_%H-%M-%S"))
+                x=int(str(frame[23:].find(' ff d8 ')))
+                with open('out_image_'+str(name)+'.jpg', 'wb') as out_file:
+                    out_file.write(int(frame[23+x:].replace(' ', ''),16).to_bytes(length=int(len(frame[23+x:].replace(' ', ''))/2), byteorder='big'))
+                with open('data.ts', 'w') as o:
+                    o.write('out_image_'+str(name)+'.jpg')
+            elif(str(img_sync) == str('02 00 3e 05') and int(str(frame[23:].find(' ff d9 '))) <= int(0)):    
+                offset=bitstring.BitStream(hex=str(frame[15:23])).read('uintle')
+                try:
+                    with open('out_image_'+str(name)+'.jpg', 'r+b') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+                except FileNotFoundError:
+                    first_offset=offset
+                    with open('out_image_'+str(name)+'.jpg', 'wb') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+            if(int(str(frame[23:].find(' ff d9 '))) >= int(0)):
+                offset=bitstring.BitStream(hex=str(frame[15:23])).read('uintle')
+                try:
+                    with open('out_image_'+str(name)+'.jpg', 'r+b') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+                except FileNotFoundError:
+                    first_offset=offset
+                    with open('out_image_'+str(name)+'.jpg', 'wb') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+                name=str(time.strftime("%m-%d_%H-%M-%S"))
+
+        elif(str(img_sync) == str('02 00 3e 20')):
+            if(int(str(frame.find(' ff d8 ff '))) >= int(0)):
+                first_offset=bitstring.BitStream(hex=str(frame[15:23])).read('uintle')
+                name=str(time.strftime("%m-%d_%H-%M-%S"))
+                x=int(str(frame[23:].find(' ff d8 ')))
+                with open('Hout_image_'+str(name)+'.jpg', 'wb') as out_file:
+                    out_file.write(int(frame[23+x:].replace(' ', ''),16).to_bytes(length=int(len(frame[23+x:].replace(' ', ''))/2), byteorder='big'))
+                with open('data.ts', 'w') as o:
+                    o.write('Hout_image_'+str(name)+'.jpg')
+            elif(str(img_sync) == str('02 00 3e 20') and int(str(frame[23:].find(' ff d9 '))) <= int(0)):
+                offset=bitstring.BitStream(hex=str(frame[15:23])).read('uintle')
+                try:
+                    with open('out_image_'+str(name)+'.jpg', 'r+b') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+                except FileNotFoundError:
+                    first_offset=offset
+                    with open('out_image_'+str(name)+'.jpg', 'wb') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+            if(int(str(frame[23:].find(' ff d9 '))) >= int(0)):
+                offset=bitstring.BitStream(hex=str(frame[15:23])).read('uintle')
+                try:
+                    with open('out_image_'+str(name)+'.jpg', 'r+b') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+                except FileNotFoundError:
+                    first_offset=offset
+                    with open('out_image_'+str(name)+'.jpg', 'wb') as out_file:
+                        out_file.seek(offset-first_offset)
+                        out_file.write(int(frame[23:].replace(' ', ''),16).to_bytes(length=int(len(frame[23:].replace(' ', ''))/2), byteorder='big'))
+                name=str(time.strftime("%m-%d_%H-%M-%S"))
         if(str(frame[:18]) == str('84 8a 82 86 9e 9c ')):
             telemetry_decoder(data=str(str(frame).replace(' ', '')))
 
 if(__name__=='__main__'):
+    name=f'Delete_this_file_{time.strftime("%m-%d_%H-%M")}'
     ip=parser.parse_args().ip
     port=parser.parse_args().port
     s=start_socket(ip=ip, port=int(port))
     agw_connect(s=s)
-    main(s=s)
+    main(s=s,name=name)
